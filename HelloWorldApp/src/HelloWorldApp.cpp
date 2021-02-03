2901dfdddfd94759434dc44c732a38687941af52
zyazyaG <agulyamova14@gmail.com>
2021-02-03 14:37:56 -0500

    Code in C++

---
+++ HelloWorldApp/src/HelloWorldApp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorldApp.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
